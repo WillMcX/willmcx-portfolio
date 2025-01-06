709b0cf669cb6991ad1ba3bca8bcaf4a65534c6c
WillMcX <wmccoy104609@gmail.com>
2025-01-05 19:55:56 -0500

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    assetPrefix: isProd ? '/my-portfolio-site/' : '',
+    output: 'export',
+    assetPrefix: isProd ? '/portfolio-site/' : '',
+    basePath: '/portfolio-site',
     images: {
         unoptimized: true,
     },
+    trailingSlash: true,
 };
